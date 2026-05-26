4e465a5187ea16802f77f1164f69ddfaa9d2f618
damianprog <damianpukszta@gmail.com>
2026-05-26 11:36:23 +0200

    Rotate array reverse

--- rotate-array.js
+++ rotate-array.js
@@ -36,22 +36,42 @@ var rotate = function (nums, k) {
   // ======================================================================================
   // Wersja kanoniczna podejście cyklowe
 
+  // const n = nums.length;
+  // k = k % n; // normalizacja
+  // let count = 0;
+
+  // for (let start = 0; count < n; start++) {
+  //   // wolę while (swapCount < nums.length)
+  //   let current = start;
+  //   let carry = nums[start];
+
+  //   do {
+  //     const next = (current + k) % n;
+  //     [nums[next], carry] = [carry, nums[next]]; // odłóż carry, podnieś to co tam było
+  //     current = next;
+  //     count++;
+  //   } while (current !== start);
+  // }
+
+  // ======================================================================================
+  // Wersja z reverse
+
+  // Czy powinienem modyfikować oryginalny array czy może zrobić kopie?
+  const reverse = (arr, left, right) => {
+    while (left < right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+
+      left++;
+      right--;
+    }
+  };
+
   const n = nums.length;
-  k = k % n; // normalizacja
-  let count = 0;
+  k = k % n;
 
-  for (let start = 0; count < n; start++) {
-    // wolę while (swapCount < nums.length)
-    let current = start;
-    let carry = nums[start];
-
-    do {
-      const next = (current + k) % n;
-      [nums[next], carry] = [carry, nums[next]]; // odłóż carry, podnieś to co tam było
-      current = next;
-      count++;
-    } while (current !== start);
-  }
+  reverse(nums, 0, n - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, n - 1);
 
   // ======================================================================================
 };
