e23e3675429155fe96b160b21771e2d270034556
damianprog <damianpukszta@gmail.com>
2026-05-30 19:55:19 +0200

    Jump game 2

--- jump-game-2.js
+++ jump-game-2.js
@@ -2,4 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function (nums) {};
+var jump = function (nums) {
+  if (nums.length === 1) return 0;
+
+  let maxReach = 0;
+  let jumps = 0;
+  let range = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    maxReach = Math.max(maxReach, nums[i] + i);
+
+    if (i === range) {
+      range = maxReach;
+      jumps++;
+
+      if (range >= nums.length - 1) return jumps;
+    }
+  }
+
+  return jumps;
+};
