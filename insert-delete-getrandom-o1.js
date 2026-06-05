605e5878a292767d7fc67df9b9738e5c5827f885
damianprog <damianpukszta@gmail.com>
2026-06-05 12:17:07 +0200

    Insert delete getrandom O(1) actually O(1)

--- insert-delete-getrandom-o1.js
+++ insert-delete-getrandom-o1.js
@@ -1,6 +1,5 @@
 var RandomizedSet = function () {
   this.array = [];
-  // this.set = new Set();
   this.map = new Map();
 };
 
@@ -8,18 +7,52 @@ var RandomizedSet = function () {
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function (val) {};
+RandomizedSet.prototype.insert = function (val) {
+  if (!this.map.has(val)) {
+    if (this.map.size < this.array.length) {
+      this.array[this.map.size] = val;
+      this.map.set(val, this.map.size);
+    } else {
+      this.array.push(val);
+      this.map.set(val, this.array.length - 1);
+    }
+
+    return true;
+  } else {
+    return false;
+  }
+};
 
 /**
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function (val) {};
+RandomizedSet.prototype.remove = function (val) {
+  if (this.map.has(val)) {
+    const index = this.map.get(val);
+
+    this.map.set(this.array[this.map.size - 1], index);
+
+    [this.array[index], this.array[this.map.size - 1]] = [
+      this.array[this.map.size - 1],
+      this.array[index],
+    ];
+
+    this.map.delete(val);
+    return true;
+  } else {
+    return false;
+  }
+};
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function () {};
+RandomizedSet.prototype.getRandom = function () {
+  const randomIndex = Math.floor(Math.random() * this.map.size);
+
+  return this.array[randomIndex];
+};
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
