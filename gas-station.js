caf966066bd2f3f4037b28c4f718d4ef1a91a966
damianprog <damianpukszta@gmail.com>
2026-06-11 11:01:46 +0200

    Gas station

---
+++ gas-station.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function (gas, cost) {
+  const gasSum = gas.reduce((sum, currentValue) => sum + currentValue, 0);
+  const costSum = cost.reduce((sum, currentValue) => sum + currentValue, 0);
+
+  if (gasSum < costSum) return -1;
+
+  let start = 0;
+  let currentTank = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    currentTank += gas[i] - cost[i];
+    if (currentTank < 0) {
+      start = i + 1;
+      currentTank = 0;
+    }
+  }
+
+  return start;
+};
+
+const gas = [4, 5, 2, 6, 5, 3];
+
+const cost = [3, 2, 7, 3, 2, 9];
+
+console.log(canCompleteCircuit(gas, cost));
