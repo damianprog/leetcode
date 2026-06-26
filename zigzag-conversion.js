075159376f3a016a7baa1a6f10d6638d1b509793
damianprog <damianpukszta@gmail.com>
2026-06-26 12:32:53 +0200

    Zigzag conversion draft

---
+++ zigzag-conversion.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  let pointer = 0;
+
+  for (let i = 0; i < s.length; i++) {}
+};
