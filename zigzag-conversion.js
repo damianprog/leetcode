72aa3459056e0f5199541d779535ad1bc43f54a9
damianprog <damianpukszta@gmail.com>
2026-06-29 10:59:48 +0200

    Zigzag conversion first solution

--- zigzag-conversion.js
+++ zigzag-conversion.js
@@ -3,8 +3,26 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function (s, numRows) {
-  let pointer = 0;
+const convert = function (s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
 
-  for (let i = 0; i < s.length; i++) {}
+  const zigzagRows = new Array(numRows).fill("");
+
+  let pointer = 0;
+  let isPointerGrowing = true;
+
+  for (let i = 0; i < s.length; i++) {
+    zigzagRows[pointer] = zigzagRows[pointer] + s[i];
+
+    pointer = isPointerGrowing ? pointer + 1 : pointer - 1;
+    if (pointer === numRows - 1) {
+      isPointerGrowing = false;
+    } else if (pointer === 0 && isPointerGrowing === false) {
+      isPointerGrowing = true;
+    }
+  }
+
+  return zigzagRows.join("");
 };
