ef233484daac31d7402fb1ec56a0ae84186b31f2
damianprog <34951410+damianprog@users.noreply.github.com>
2026-07-16 21:05:23 +0200

    Update candy.js

--- candy.js
+++ candy.js
@@ -10,7 +10,7 @@ var candy = function (ratings) {
       candy[i] = Math.max(candy[i], candy[i - 1] + 1);
     }
   }
-
+// test
   for (let i = candy.length - 2; i >= 0; i--) {
     if (ratings[i] > ratings[i + 1]) {
       candy[i] = Math.max(candy[i], candy[i + 1] + 1);
