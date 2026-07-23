991fe0734d04933d91c48520123a87d8148ee782
damianprog <damianpukszta@gmail.com>
2026-07-23 19:46:15 +0200

    Two Sum II Two pointers

--- two-sum-ii-input-array-is-sorted.js
+++ two-sum-ii-input-array-is-sorted.js
@@ -4,24 +4,40 @@
  * @return {number[]}
  */
 const twoSum = function (numbers, target) {
-  for (let i = 0; i < numbers.length - 1; i++) {
-    const wanted = target - numbers[i];
+  // for (let i = 0; i < numbers.length - 1; i++) {
+  //   const wanted = target - numbers[i];
 
-    let lo = i + 1;
-    let hi = numbers.length - 1;
+  //   let lo = i + 1;
+  //   let hi = numbers.length - 1;
 
-    while (lo <= hi) {
-      const mid = Math.floor((lo + hi) / 2);
+  //   while (lo <= hi) {
+  //     const mid = Math.floor((lo + hi) / 2);
 
-      if (numbers[mid] === wanted) {
-        return [i + 1, mid + 1];
-      } else if (numbers[mid] > wanted) {
-        hi = mid - 1;
-      } else {
-        lo = mid + 1;
-      }
+  //     if (numbers[mid] === wanted) {
+  //       return [i + 1, mid + 1];
+  //     } else if (numbers[mid] > wanted) {
+  //       hi = mid - 1;
+  //     } else {
+  //       lo = mid + 1;
+  //     }
+  //   }
+  // }
+
+  let head = 0;
+  let tail = numbers.length - 1;
+
+  while (head < tail) {
+    const sum = numbers[head] + numbers[tail];
+    if (sum > target) {
+      tail--;
+    } else if (sum < target) {
+      head++;
+    } else {
+      return [head + 1, tail + 1];
     }
   }
+
+  return [-1, -1];
 };
 
 const numbers = [-5, -3, 0, 2, 4, 6, 8];
