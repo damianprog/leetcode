f6afe08a98b4d2a6cdd6b171bc08a4de3b05cc63
damianprog <damianpukszta@gmail.com>
2026-08-09 10:16:15 +0200

    3sum canonical

--- 3sum.js
+++ 3sum.js
@@ -3,33 +3,56 @@
  * @return {number[][]}
  */
 const threeSum = function (nums) {
-  const triplets = [];
-
-  const tripletsStrings = new Set();
+  // const triplets = [];
+  // const tripletsStrings = new Set();
+  // nums.sort((a, b) => a - b);
+  // for (let i = 0; i < nums.length; i++) {
+  //   const target = -1 * nums[i];
+  //   const numsSpotted = new Set();
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     const wanted = target - nums[j];
+  //     if (numsSpotted.has(wanted)) {
+  //       const tripletString = `${nums[i]},${nums[j]},${wanted}`;
+  //       if (!tripletsStrings.has(tripletString)) {
+  //         triplets.push([nums[i], nums[j], wanted]);
+  //         tripletsStrings.add(tripletString);
+  //       }
+  //     } else {
+  //       numsSpotted.add(nums[j]);
+  //     }
+  //   }
+  // }
+  // return triplets;
+  // =========================================================================================
+  // Wersja idiomatyczna Sort + Zbiegające się two pointers
 
   nums.sort((a, b) => a - b);
+  const res = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    const target = -1 * nums[i];
-    const numsSpotted = new Set();
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break; // early exit: dalej same dodatnie, suma nigdy 0
+    if (i > 0 && nums[i] === nums[i - 1]) continue; // pomiń powtórzone x
 
-    for (let j = i + 1; j < nums.length; j++) {
-      const wanted = target - nums[j];
-
-      if (numsSpotted.has(wanted)) {
-        const tripletString = `${nums[i]},${nums[j]},${wanted}`;
-
-        if (!tripletsStrings.has(tripletString)) {
-          triplets.push([nums[i], nums[j], wanted]);
-          tripletsStrings.add(tripletString);
-        }
+    let lo = i + 1,
+      hi = nums.length - 1;
+    while (lo < hi) {
+      const sum = nums[i] + nums[lo] + nums[hi];
+      if (sum === 0) {
+        res.push([nums[i], nums[lo], nums[hi]]);
+        lo++;
+        hi--;
+        while (lo < hi && nums[lo] === nums[lo - 1]) lo++; // pomiń powtórzone od lewej
+        while (lo < hi && nums[hi] === nums[hi + 1]) hi--; // pomiń powtórzone od prawej
+      } else if (sum < 0) {
+        lo++; // za mało → podnieś dolną granicę
       } else {
-        numsSpotted.add(nums[j]);
+        hi--; // za dużo → obniż górną granicę
       }
     }
   }
+  return res;
 
-  return triplets;
+  // =========================================================================================
 };
 
 // const nums = [-1, 0, 1, 2, -1, -4];
