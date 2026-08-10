4c80a488071239f40c726b9276ea9858e5e7c866
damianprog <damianpukszta@gmail.com>
2026-08-10 11:16:46 +0200

    Minimum size subarray sum not finished

---
+++ minimum-size-subarray-sum.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let windowSize = 1;
+
+  while (windowSize <= nums.length) {
+    let windowSum = 0;
+
+    for (let i = 0; i < windowSize; i++) {
+      windowSum += nums[i];
+    }
+
+    if (windowSum === target) {
+      return windowSize;
+    }
+
+    let left = 0;
+    let right = windowSize - 1;
+
+    while (right < nums.length) {
+      windowSum -= nums[left];
+      windowSum += nums[right + 1];
+
+      if (windowSum === target) {
+        return windowSize;
+      }
+
+      left++;
+      right++;
+    }
+
+    windowSize++;
+  }
+};
