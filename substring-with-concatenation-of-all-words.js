c346ea40a8a8731eb59edbeb9be95c1162d4a906
damianprog <damianpukszta@gmail.com>
2026-08-20 11:07:58 +0200

    Substring with concatenation of all words started and deduction

--- substring-with-concatenation-of-all-words.js
+++ substring-with-concatenation-of-all-words.js
@@ -3,4 +3,15 @@
  * @param {string[]} words
  * @return {number[]}
  */
-const findSubstring = function (s, words) {};
+const findSubstring = function (s, words) {
+  const wordsQuantities = new Map();
+
+  words.forEach((word) => {
+    const wordQty = wordsQuantities.get(word);
+    wordsQuantities.set(word, wordQty ? wordQty + 1 : 1);
+  });
+
+  const allWordsCharsQty = words[0].length * words.length;
+
+  for (let i = 0; i < s.length; i++) {}
+};
