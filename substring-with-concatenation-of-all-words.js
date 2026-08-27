884cf45cc1793657fce2877746f2be5ad330f5dd
damianprog <damianpukszta@gmail.com>
2026-08-27 20:53:18 +0200

    Substring with concatenation of all words optimalization deduction

--- substring-with-concatenation-of-all-words.js
+++ substring-with-concatenation-of-all-words.js
@@ -4,6 +4,44 @@
  * @return {number[]}
  */
 const findSubstring = function (s, words) {
+  // const wordsQuantities = new Map();
+
+  // words.forEach((word) => {
+  //   const wordQty = wordsQuantities.get(word);
+  //   wordsQuantities.set(word, (wordQty ?? 0) + 1);
+  // });
+
+  // const permutationLength = words[0].length * words.length;
+
+  // const result = [];
+
+  // for (let i = 0; i <= s.length - permutationLength; i++) {
+  //   let frag = "";
+  //   const wordsQuantitiesCopy = new Map(wordsQuantities);
+  //   for (let j = 0; j < permutationLength; j++) {
+  //     frag += s[i + j];
+
+  //     if (frag.length === words[0].length) {
+  //       const wordQuantity = wordsQuantitiesCopy.get(frag);
+  //       if (wordQuantity) {
+  //         wordsQuantitiesCopy.set(frag, wordQuantity - 1);
+  //       } else {
+  //         break;
+  //       }
+  //       frag = "";
+  //     }
+
+  //     if (j === permutationLength - 1) {
+  //       result.push(i);
+  //     }
+  //   }
+  // }
+
+  // return result;
+
+  // =====================================================================================
+  // Wersja bardziej optymalna [tu wstawić jej big O]
+
   const wordsQuantities = new Map();
 
   words.forEach((word) => {
@@ -11,33 +49,7 @@ const findSubstring = function (s, words) {
     wordsQuantities.set(word, (wordQty ?? 0) + 1);
   });
 
-  const permutationLength = words[0].length * words.length;
-
-  const result = [];
-
-  for (let i = 0; i <= s.length - permutationLength; i++) {
-    let frag = "";
-    const wordsQuantitiesCopy = new Map(wordsQuantities);
-    for (let j = 0; j < permutationLength; j++) {
-      frag += s[i + j];
-
-      if (frag.length === words[0].length) {
-        const wordQuantity = wordsQuantitiesCopy.get(frag);
-        if (wordQuantity) {
-          wordsQuantitiesCopy.set(frag, wordQuantity - 1);
-        } else {
-          break;
-        }
-        frag = "";
-      }
-
-      if (j === permutationLength - 1) {
-        result.push(i);
-      }
-    }
-  }
-
-  return result;
+  for (let offset = 0; offset < words[0].length; offset++) {}
 };
 
 const s = "barfoothefoobarman";
