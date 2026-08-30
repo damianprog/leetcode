bb759df6479e4ca09827a34e0a1477f087707b3f
damianprog <damianpukszta@gmail.com>
2026-08-30 16:31:52 +0200

    Substring with concatenation of all words optimized, all tests pass but need some refactor

--- substring-with-concatenation-of-all-words.js
+++ substring-with-concatenation-of-all-words.js
@@ -49,6 +49,8 @@ const findSubstring = function (s, words) {
     wordsQuantities.set(word, (wordQty ?? 0) + 1);
   });
 
+  const result = [];
+
   for (let offset = 0; offset < words[0].length; offset++) {
     let left = offset;
     let seen = new Map();
@@ -56,33 +58,71 @@ const findSubstring = function (s, words) {
 
     for (
       let right = offset;
-      right + words[0].length <= n;
+      right + words[0].length <= s.length;
       right += words[0].length
     ) {
-      const chunk = s.slice(right, right + L);
+      const chunk = s.slice(right, right + words[0].length);
 
       const wordQty = wordsQuantities.get(chunk);
 
       if (!wordQty) {
-        left = right + L;
+        left = right + words[0].length;
         seen = new Map();
         count = 0;
       } else if (!seen.has(chunk) || seen.get(chunk) + 1 <= wordQty) {
         seen.set(chunk, (seen.get(chunk) ?? 0) + 1);
+        count++;
+        if (count === words.length) {
+          result.push(left);
+          const leftmostChunk = s.slice(left, left + words[0].length);
+          seen.set(leftmostChunk, seen.get(leftmostChunk) - 1);
+          left += words[0].length;
+          count--;
+        }
       } else {
-        let isPrevChunkRemoved = false;
+        seen.set(chunk, seen.get(chunk) + 1);
+        count++;
+        let chunkRemoved = "";
 
-        while (isPrevChunkRemoved) {
-          const currentChunk = s.slice(left, left + words[0].length);
+        while (chunkRemoved !== chunk) {
+          chunkRemoved = s.slice(left, left + words[0].length);
 
-          seen.set(currentChunk, seen.get(currentChunk) - 1); // także w seen może być 0
+          left += words[0].length;
+
+          count--;
+
+          seen.set(chunkRemoved, seen.get(chunkRemoved) - 1);
         }
       }
+
+      // const expected = (right - left) / words[0].length + 1;
+      // if (count !== expected) {
+      //   console.log("INVARIANT BROKEN", {
+      //     right,
+      //     left,
+      //     count,
+      //     expected,
+      //     chunk,
+      //     seen,
+      //   });
+      // }
     }
   }
+
+  return result;
 };
 
-const s = "barfoothefoobarman";
+// const s = "barfoothefoobarman";
+// const s = "thefoobarman";
+// const s = "thefoobar";
+// const s = "thefoobar";
+
+// const words = ["foo", "bar"];
+
+// const s = "barfoofoobarthefoobarman";
+// const words = ["bar", "foo", "the"];
+
+const s = "foofoobar";
 const words = ["foo", "bar"];
 
 console.log(findSubstring(s, words));
