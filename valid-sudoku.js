25ae084ec8d5bd81792d5c438594766e3687e1e6
damianprog <damianpukszta@gmail.com>
2026-09-06 10:52:42 +0200

    Valid sudoku optimization and refactor in progress

--- valid-sudoku.js
+++ valid-sudoku.js
@@ -2,55 +2,88 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {
+const isValidSudoku = function (board) {
+  // for (let i = 0; i < 9; i++) {
+  //   const numbers = new Set();
+  //   for (let j = 0; j < 9; j++) {
+  //     if (board[i][j] !== ".") {
+  //       if (numbers.has(board[i][j])) {
+  //         return false;
+  //       } else {
+  //         numbers.add(board[i][j]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // for (let i = 0; i < 9; i++) {
+  //   const numbers = new Set();
+  //   for (let j = 0; j < 9; j++) {
+  //     if (board[j][i] !== ".") {
+  //       if (numbers.has(board[j][i])) {
+  //         return false;
+  //       } else {
+  //         numbers.add(board[j][i]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // for (let i = 0; i < 3; i++) {
+  //   for (let j = 0; j < 3; j++) {
+  //     const offsetY = i * 3;
+  //     const offsetX = j * 3;
+
+  //     const numbers = new Set();
+
+  //     for (let k = 0; k < 3; k++) {
+  //       for (let l = 0; l < 3; l++) {
+  //         if (board[offsetY + k][offsetX + l] !== ".") {
+  //           if (numbers.has(board[offsetY + k][offsetX + l])) {
+  //             return false;
+  //           } else {
+  //             numbers.add(board[offsetY + k][offsetX + l]);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // return true;
+
+  // ======================================================================================
+
+  const rowsSets = [];
+  const colsSets = [];
+  const boxesSets = [];
+
+  [rowsSets, colsSets, boxesSets].forEach((sets) => {
+    for (let i = 0; i < 9; i++) {
+      sets.push(new Set());
+    }
+  });
+
   for (let i = 0; i < 9; i++) {
-    const numbers = new Set();
     for (let j = 0; j < 9; j++) {
-      if (board[i][j] !== ".") {
-        if (numbers.has(board[i][j])) {
+      const cell = board[i][j];
+      if (cell !== ".") {
+        if (rowsSets[i].has(cell)) {
           return false;
         } else {
-          numbers.add(board[i][j]);
+          rowsSets[i].add(cell);
         }
-      }
-    }
-  }
 
-  for (let i = 0; i < 9; i++) {
-    const numbers = new Set();
-    for (let j = 0; j < 9; j++) {
-      if (board[j][i] !== ".") {
-        if (numbers.has(board[j][i])) {
+        if (colsSets[j].has(cell)) {
           return false;
         } else {
-          numbers.add(board[j][i]);
+          colsSets[j].add(cell);
         }
       }
     }
   }
 
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      const offsetY = i * 3;
-      const offsetX = j * 3;
-
-      const numbers = new Set();
-
-      for (let k = 0; k < 3; k++) {
-        for (let l = 0; l < 3; l++) {
-          if (board[offsetY + k][offsetX + l] !== ".") {
-            if (numbers.has(board[offsetY + k][offsetX + l])) {
-              return false;
-            } else {
-              numbers.add(board[offsetY + k][offsetX + l]);
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return true;
+  // ======================================================================================
 };
 
 // const board = [
