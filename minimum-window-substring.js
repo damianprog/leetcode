31d41001f7658af6aff7ba6d102fd89f4100ee0d
damianprog <damianpukszta@gmail.com>
2026-09-09 10:49:32 +0200

    Minimum window substring in progress and deduction

--- minimum-window-substring.js
+++ minimum-window-substring.js
@@ -3,4 +3,36 @@
  * @param {string} t
  * @return {string}
  */
-const minWindow = function (s, t) {};
+const minWindow = function (s, t) {
+  const tUniqueChars = new Set();
+
+  for (const char of t) {
+    tUniqueChars.add(char);
+  }
+
+  let currentSubstring = "";
+  let result = "";
+  let tCharsCounter = 0;
+  let substringUniqueCharsFromT = new Set();
+
+  for (let i = 0; i < s.length; i++) {
+    if (
+      substringUniqueCharsFromT.size < tUniqueChars.size &&
+      (result.length === 0 || currentSubstring.length < result.length - 1)
+    ) {
+      currentSubstring += s[i];
+
+      if (tUniqueChars.has(s[i])) {
+        substringUniqueCharsFromT.add(s[i]);
+      }
+    } else {
+      if (substringUniqueCharsFromT.size < tUniqueChars.size) {
+        result = currentSubstring;
+      } else {
+      }
+
+      currentSubstring = "";
+      tCharsCounter = 0;
+    }
+  }
+};
