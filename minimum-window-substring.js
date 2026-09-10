193550c0d4c8c5e6119bc6ad1d3298ec1a3d8284
damianprog <damianpukszta@gmail.com>
2026-09-10 10:30:33 +0200

    Minimum window substring switched back from set to map and deduction

--- minimum-window-substring.js
+++ minimum-window-substring.js
@@ -4,34 +4,64 @@
  * @return {string}
  */
 const minWindow = function (s, t) {
-  const tUniqueChars = new Set();
+  // const tUniqueChars = new Set(t);
+
+  // let currentSubstring = "";
+  // let result = "";
+  // let tCharsCounter = 0;
+  // let substringUniqueCharsFromT = new Set();
+
+  // for (let i = 0; i < s.length; i++) {
+  //   if (
+  //     substringUniqueCharsFromT.size < tUniqueChars.size &&
+  //     (result.length === 0 || currentSubstring.length < result.length - 1)
+  //   ) {
+  //     currentSubstring += s[i];
+
+  //     if (tUniqueChars.has(s[i])) {
+  //       substringUniqueCharsFromT.add(s[i]);
+  //     }
+  //   } else {
+  //     if (substringUniqueCharsFromT.size === tUniqueChars.size) {
+  //       result = currentSubstring;
+  //     } else {
+  //     }
+
+  //     currentSubstring = "";
+  //     tCharsCounter = 0;
+  //   }
+  // }
+
+  const tCharsQuantities = new Map();
 
   for (const char of t) {
-    tUniqueChars.add(char);
+    tCharsQuantities.set(char, (tCharsQuantities.get(char) ?? 0) + 1);
   }
 
   let currentSubstring = "";
   let result = "";
   let tCharsCounter = 0;
-  let substringUniqueCharsFromT = new Set();
 
   for (let i = 0; i < s.length; i++) {
     if (
-      substringUniqueCharsFromT.size < tUniqueChars.size &&
+      tCharsCounter < t.length &&
       (result.length === 0 || currentSubstring.length < result.length - 1)
     ) {
       currentSubstring += s[i];
 
-      if (tUniqueChars.has(s[i])) {
-        substringUniqueCharsFromT.add(s[i]);
+      const tCharQty = tCharsQuantities.get(s[i]);
+
+      if (tCharQty) {
+        tCharsQuantities.set(s[i], tCharQty - 1);
+        tCharsCounter++;
       }
     } else {
-      if (substringUniqueCharsFromT.size < tUniqueChars.size) {
+      if (result.length === 0 || currentSubstring.length < result.length - 1) {
         result = currentSubstring;
       } else {
       }
 
-      currentSubstring = "";
+      // currentSubstring = "";
       tCharsCounter = 0;
     }
   }
