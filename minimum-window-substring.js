bbf0a375a8a728ea678391a594ba89401092d779
damianprog <damianpukszta@gmail.com>
2026-09-11 12:10:58 +0200

    Minimum window substring needs debugging and deduction

--- minimum-window-substring.js
+++ minimum-window-substring.js
@@ -4,34 +4,6 @@
  * @return {string}
  */
 const minWindow = function (s, t) {
-  // const tUniqueChars = new Set(t);
-
-  // let currentSubstring = "";
-  // let result = "";
-  // let tCharsCounter = 0;
-  // let substringUniqueCharsFromT = new Set();
-
-  // for (let i = 0; i < s.length; i++) {
-  //   if (
-  //     substringUniqueCharsFromT.size < tUniqueChars.size &&
-  //     (result.length === 0 || currentSubstring.length < result.length - 1)
-  //   ) {
-  //     currentSubstring += s[i];
-
-  //     if (tUniqueChars.has(s[i])) {
-  //       substringUniqueCharsFromT.add(s[i]);
-  //     }
-  //   } else {
-  //     if (substringUniqueCharsFromT.size === tUniqueChars.size) {
-  //       result = currentSubstring;
-  //     } else {
-  //     }
-
-  //     currentSubstring = "";
-  //     tCharsCounter = 0;
-  //   }
-  // }
-
   const tCharsQuantities = new Map();
 
   for (const char of t) {
@@ -41,28 +13,62 @@ const minWindow = function (s, t) {
   let currentSubstring = "";
   let result = "";
   let tCharsCounter = 0;
+  let left = 0;
 
   for (let i = 0; i < s.length; i++) {
+    currentSubstring += s[i];
+
+    const tCharQty = tCharsQuantities.get(s[i]);
+
+    if (tCharQty) {
+      tCharsQuantities.set(s[i], tCharQty - 1);
+      tCharsCounter++;
+    }
+
     if (
-      tCharsCounter < t.length &&
-      (result.length === 0 || currentSubstring.length < result.length - 1)
+      tCharsCounter === t.length ||
+      (result.length > 0 && currentSubstring.length === result.length - 1)
     ) {
-      currentSubstring += s[i];
-
-      const tCharQty = tCharsQuantities.get(s[i]);
-
-      if (tCharQty) {
-        tCharsQuantities.set(s[i], tCharQty - 1);
-        tCharsCounter++;
-      }
-    } else {
-      if (result.length === 0 || currentSubstring.length < result.length - 1) {
+      if (tCharsCounter === t.length) {
         result = currentSubstring;
-      } else {
       }
 
-      // currentSubstring = "";
-      tCharsCounter = 0;
+      const leftCharTQty = tCharsQuantities.get(s[left]);
+      if (leftCharTQty || leftCharTQty === 0) {
+        tCharsQuantities.set(s[left], leftCharTQty + 1);
+        tCharsCounter--;
+      }
+
+      left++;
+
+      while (left < i && !tCharsQuantities.has(s[left])) {
+        left++;
+      }
+
+      currentSubstring = s.slice(left, i + 1);
     }
   }
+
+  return result;
 };
+
+// const s = "ADOBECODEBANC";
+// // const s = "BANC";
+// const t = "ABC";
+
+// const s = "a";
+// const t = "a";
+
+// const s = "a";
+// const t = "aa";
+
+// const s = "ab";
+// const t = "b";
+
+// const s = "aabc";
+// const t = "abbc";
+
+const s = "abc";
+const t = "b";
+
+console.log(minWindow(s, t));
