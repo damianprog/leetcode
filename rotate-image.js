39b62cf0ea024dca48b0c728fb4a8f4b87001cb9
damianprog <damianpukszta@gmail.com>
2026-09-25 09:23:00 +0200

    Rotate image debugging

--- rotate-image.js
+++ rotate-image.js
@@ -6,16 +6,25 @@ const rotate = function (matrix) {
   const n = matrix.length - 1;
 
   for (let laps = 0; laps < Math.floor(matrix[0].length / 2); laps++) {
-    for (let i = 0; i < matrix[0].length - 1; i++) {
-      for (let j = 0; j < 4; j++) {
+    for (let i = 0; i < matrix[0].length - laps - 1; i++) {
+      for (let j = 0; j < 3; j++) {
         if (j === 0) {
-          [matrix[0][0], matrix[0][n]] = [matrix[0][n], matrix[0][0]];
+          [matrix[laps][laps + i], matrix[i][n - laps]] = [
+            matrix[i][n - laps],
+            matrix[laps][laps + i],
+          ];
         }
         if (j === 1) {
-          [matrix[0][0], matrix[n][n]] = [matrix[n][n], matrix[0][0]];
+          [matrix[laps][laps + i], matrix[n - i][n - laps]] = [
+            matrix[n - i][n - laps],
+            matrix[laps][laps + i],
+          ];
         }
         if (j === 2) {
-          [matrix[0][0], matrix[n][0]] = [matrix[n][0], matrix[0][0]];
+          [matrix[laps][laps + i], matrix[n - i][laps]] = [
+            matrix[n - i][laps],
+            matrix[laps][laps + i],
+          ];
         }
       }
     }
