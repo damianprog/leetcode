a4e05fbea15b305da0775bda28af8fd73f664a60
damianprog <damianpukszta@gmail.com>
2026-09-25 12:53:50 +0200

    Rotate image one lap working

--- rotate-image.js
+++ rotate-image.js
@@ -5,7 +5,8 @@
 const rotate = function (matrix) {
   const n = matrix.length - 1;
 
-  for (let laps = 0; laps < Math.floor(matrix[0].length / 2); laps++) {
+  // for (let laps = 0; laps < Math.floor(matrix[0].length / 2); laps++) {
+  for (let laps = 0; laps < 1; laps++) {
     for (let i = 0; i < matrix[0].length - laps - 1; i++) {
       for (let j = 0; j < 3; j++) {
         if (j === 0) {
@@ -15,8 +16,8 @@ const rotate = function (matrix) {
           ];
         }
         if (j === 1) {
-          [matrix[laps][laps + i], matrix[n - i][n - laps]] = [
-            matrix[n - i][n - laps],
+          [matrix[laps][laps + i], matrix[n - laps][n - i]] = [
+            matrix[n - laps][n - i],
             matrix[laps][laps + i],
           ];
         }
@@ -30,3 +31,20 @@ const rotate = function (matrix) {
     }
   }
 };
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+const matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
+
+rotate(matrix);
+
+console.log(matrix);
