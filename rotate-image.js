750aedce295e424c3ccb1e185ff7669d7d45d66f
damianprog <damianpukszta@gmail.com>
2026-09-26 13:50:39 +0200

    Rotate image refactor, useless loop removed

--- rotate-image.js
+++ rotate-image.js
@@ -7,42 +7,34 @@ const rotate = function (matrix) {
 
   for (let laps = 0; laps < Math.floor(matrix[0].length / 2); laps++) {
     for (let i = 0; i < matrix[0].length - laps * 2 - 1; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (j === 0) {
-          [matrix[laps][laps + i], matrix[i + laps][n - laps]] = [
-            matrix[i + laps][n - laps],
-            matrix[laps][laps + i],
-          ];
-        }
-        if (j === 1) {
-          [matrix[laps][laps + i], matrix[n - laps][n - i - laps]] = [
-            matrix[n - laps][n - i - laps],
-            matrix[laps][laps + i],
-          ];
-        }
-        if (j === 2) {
-          [matrix[laps][laps + i], matrix[n - i - laps][laps]] = [
-            matrix[n - i - laps][laps],
-            matrix[laps][laps + i],
-          ];
-        }
-      }
+      [matrix[laps][laps + i], matrix[i + laps][n - laps]] = [
+        matrix[i + laps][n - laps],
+        matrix[laps][laps + i],
+      ];
+      [matrix[laps][laps + i], matrix[n - laps][n - i - laps]] = [
+        matrix[n - laps][n - i - laps],
+        matrix[laps][laps + i],
+      ];
+      [matrix[laps][laps + i], matrix[n - i - laps][laps]] = [
+        matrix[n - i - laps][laps],
+        matrix[laps][laps + i],
+      ];
     }
   }
 };
 
-const matrix = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
 // const matrix = [
-//   [5, 1, 9, 11],
-//   [2, 4, 8, 10],
-//   [13, 3, 6, 7],
-//   [15, 14, 12, 16],
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
 // ];
+
+const matrix = [
+  [5, 1, 9, 11],
+  [2, 4, 8, 10],
+  [13, 3, 6, 7],
+  [15, 14, 12, 16],
+];
 
 rotate(matrix);
 
